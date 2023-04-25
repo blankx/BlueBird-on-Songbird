a6ca75c7f150d0d4ea45b878a68ac15fe887c852
blankx <jimminycracker@hotmail.com>
2023-04-25 16:38:49 -0700

    Create ConnectWallet.vue

---
+++ components/ConnectWallet.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="connect-wallet">
+    <button @click="connectWallet" class="connect-wallet-btn">
+      Connect Wallet
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ConnectWallet',
+  methods: {
+    connectWallet() {
+      // Implement wallet connection logic here.
+    },
+  },
+};
+</script>
+
+<style scoped>
+.connect-wallet-btn {
+  background-color: #1c1c1c;
+  color: #ffffff;
+  border: none;
+  padding: 0.5rem 1rem;
+  cursor: pointer;
+}
+</style>
