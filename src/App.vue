529a516502c4080da77c1d37d586d6a65c25ca8a
blankx <jimminycracker@hotmail.com>
2023-04-25 16:34:55 -0700

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <header>
+      <h1>BlueBird dApp</h1>
+    </header>
+    <main>
+      <LinkTwitter />
+      <SendTransaction />
+      <TransactionHistory />
+      <RewardsDisplay />
+    </main>
+  </div>
+</template>
+
+<script>
+import LinkTwitter from './components/LinkTwitter.vue';
+import SendTransaction from './components/SendTransaction.vue';
+import TransactionHistory from './components/TransactionHistory.vue';
+import RewardsDisplay from './components/RewardsDisplay.vue';
+
+export default {
+  name: 'App',
+  components: {
+    LinkTwitter,
+    SendTransaction,
+    TransactionHistory,
+    RewardsDisplay,
+  },
+};
+</script>
+
+<style>
+/* Add your global styles here */
+</style>
